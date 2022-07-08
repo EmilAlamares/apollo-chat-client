114a6986784d7befaa8d08e4e25e26f04808c775
John Emanuel Alamares <alamaresjohnemanuel@gmail.com>
2022-07-09 01:24:48 +0800

    Fixed a bug when clicking a user from search results doesn't load the correct conversation in the selectedConversation state.

--- src/components/SearchResultCard.js
+++ src/components/SearchResultCard.js
@@ -1,17 +1,24 @@
 import { UserContext } from "../contexts/UserContext"
 import { useContext } from "react"
 
-const SearchResultCard = ({ result, setSelectedConversation, setOtherUser }) => {
+const SearchResultCard = ({
+  result,
+  setSelectedConversation,
+  setOtherUser,
+}) => {
   const { user } = useContext(UserContext)
   return (
     <div
       className="search-results-card flex"
       onMouseDown={() => {
-        if ((result?.conversation[0]?.usersName)?.includes(user.username))
-        setSelectedConversation(result.conversation[0]._id)
-        else
         setSelectedConversation(null)
-        setOtherUser({username: result.username, id: result._id})
+
+        result.conversation.map((res, idx) => {
+          if (res.usersName.includes(user.username)) // Checks if the user in the card has a conversation with the current user.
+          return setSelectedConversation(result.conversation[idx]._id)
+        })
+
+        setOtherUser({ username: result.username, id: result._id })
       }}
     >
       <img src={`http://localhost:8000/image/${result._id}`} alt="user" />
